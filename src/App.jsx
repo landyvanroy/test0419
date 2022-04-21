c55cb6532f98f88266f188c2763d448b59f6f5a5
Wang Ruoyu <landyvanroy@gmail.com>
2022-04-21 20:16:22 +0800

    downgrade to react17.x

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useRoutes} from 'react-router-dom';
 import routes from './config/routes';
 import MyTabBar from './components/my_tab_bar';
 import Auth from './components/auth';
+import  './App.css';
 
 function App() {
   return (
     <div>
       <Auth/>
       <MyTabBar/>
-      {useRoutes(routes)}
+      <div id="page">
+        {useRoutes(routes)}
+      </div>
     </div>
   );
 }
